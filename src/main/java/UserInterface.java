298de82a28dc689a27b0dbd6377bb3288eeea101
marbin701 <marvin07012002@gmail.com>
2026-05-11 14:03:07 -0400

    working in phase 3
    update the menu

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,12 +1,46 @@
+import java.util.Scanner;
 
 public class UserInterface {
     private Dealership dealership;
+    Scanner  scanner =  new Scanner(System.in);
+
     private void init(){
         DealershipFileMananger dfm = new DealershipFileMananger();
         this.dealership = dfm.getDealership();
-
     }
 
+    public void display (){
+        init();
+
+        boolean running = true;
+
+
+        do {
+            System.out.println("""
+                    Menu
+                    A) for display all cars
+                    B) Display cars by price
+                    X) Exit
+                    """);
+            String userInput = scanner.nextLine();
+
+            switch (userInput){
+                case "A":
+                    displayAlLCars();
+                    break;
+                case "B":
+                    DisplayCarsByPrice();
+                    break;
+                case "X":
+                    running =  false;
+                    break;
+
+            }
+
+
+
+        } while(running);
+    }
 
 
 

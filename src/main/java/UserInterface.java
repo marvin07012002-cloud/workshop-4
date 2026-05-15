30f7e68c4bfa3a64bac70a5dcd18e52b1325df01
marbin701 <marvin07012002@gmail.com>
2026-05-14 20:29:23 -0400

    working in user interface, adding the menus and process

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class UserInterface {
     private Dealership dealership;
-    Scanner  scanner =  new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
-    private void init(){
+    private void init() {
         DealershipFileMananger dfm = new DealershipFileMananger();
         this.dealership = dfm.getDealership();
     }
 
-    public void display (){
+    public void display() {
         init();
 
         boolean running = true;
@@ -24,30 +24,74 @@ public class UserInterface {
                     """);
             String userInput = scanner.nextLine();
 
-            switch (userInput){
+            switch (userInput) {
                 case "A":
                     displayAlLCarsMenu();
                     break;
                 case "B":
-                   processAddVehicles();
+                    processAddVehicles();
                     break;
                 case "C":
                     processRemoveVehicel();
                     break;
                 case "X":
-                    running =  false;
+                    running = false;
                     break;
 
             }
 
 
+        } while (running);
+    }
 
-        } while(running);
+    private void displayAlLCarsMenu() {
+        boolean running = true;
+
+        while (running) {
+            System.out.println("""
+                    Select filter to search vehicle
+                    1) Search by price
+                    2) Search by model
+                    3) Search by year
+                    4) Search by color
+                    5) Search by mileage 
+                    6) Search by vehicle type
+                    7) Search by all vehicles in the dealership
+                    8) Exit 
+                    """);
+
+            try {
+                String userInput = scanner.nextLine();
+
+                switch (userInput) {
+                    case "1":
+
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        break;
+                    case "7":
+                        break;
+                    case "8":
+
+                }
+
+            }
+        }
     }
 
 
+    private void processRemoveVehicel() {
+    }
 
-
+    private void processAddVehicles() {
+    }
 
 
 }
